6aeda1dcc172c0387a6e8f33e05d787d75ff78a2
vishnupriya-shisode <140819184+vishnupriya-shisode@users.noreply.github.com>
2026-02-04 14:24:57 +0000

    Display Card

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ let hasBlackJack = false
 let isAlive = true
 
 let message = ""
+let cardEL = document.getElementById('card-el')
+let sumEL = document.getElementById('sum-el')
+let messageEL = document.getElementById("message-el")
+console.log(messageEL)
 
 
 function startGame() {
+    cardEL.textContent = "Card : " + firstCard + " " +  secondCard
+     sumEL.textContent = "SUM : " + sum
+
     if (sum <= 20) {
         message = "do you want to draw another card"
     } else if ( sum === 21) {
-    message = "Woohoo!! You've got the Blackjack 🥳"
+    message = "Woohoo!! You've got the Blackjack"
         hasBlackJack = true
     }else{
         message = "You're out of the game"
         isAlive = false
     }
 
-    console.log(message)
+    messageEL.textContent = message
+   
 
 } 
